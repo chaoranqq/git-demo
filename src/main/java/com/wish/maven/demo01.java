45ae5a3931ae24253638826b2b7c2635a942a190
chaoran <873312539@qq.com>
2021-05-27 18:19:45 +0800

    test

---
+++ src/main/java/com/wish/maven/demo01.java
@@ -0,0 +1,11 @@
+package com.wish.maven;
+
+/**
+ * @author
+ * @create 2021-05-27 18:18
+ */
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println("案例01");
+    }
+}
